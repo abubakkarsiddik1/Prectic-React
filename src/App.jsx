800cf9d1cbff121371a1fd12e02af00348e91c62
abubakkarsiddik1 <mdabubokorsiddik283@gmail.com>
2025-10-18 20:48:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+import NavBar from './components/NavBar/NavBar'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    
+    <header>
+    <DaisyNav></DaisyNav>
+    </header>
+    <main>
+    <NavBar></NavBar>
+    </main>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    name: "About",
+    path: "/about",
+  },
+  {
+    id: 3,
+    name: "Services",
+    path: "/services",
+  },
+  {
+    id: 4,
+    name: "Products",
+    path: "/products",
+  },
+  {
+    id: 5,
+    name: "Contact",
+    path: "/contact",
+  },
+];
+
+
+const NavBar = () => {
+    return (
+        <nav>
+            {/* Ami amar moto kore dainamicali banate pari */}
+            <ul className='flex'>
+                {
+                   navLinks.map(route =><li className='mr-10'>
+                    <a href={route.path}>{route.name}</a></li> )
+                }
+            </ul>
+            {/* Ami chaile A vabe o korte pari */}
+            {/* <ul className='flex '>
+                <li className='mr-10'><a href="/">Home</a></li>
+                <li className='mr-10'><a href="/about">About</a></li>
+                <li className='mr-10'><a href="/blog">Blog</a></li>
+                <li className='mr-10'><a href="/log in">Log In</a></li>
+            </ul> */}
+        </nav>
+    );
+};
+
+export default NavBar;

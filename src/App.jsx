46b4a37df0f74058543e362ea41557dc3ce0dd9c
abubakkarsiddik1 <mdabubokorsiddik283@gmail.com>
2025-10-31 16:11:22 +0600

    MarksChart, Daisy ChartCard, App

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import DaisyNav from './components/DaisyNav/DaisyNav'
 import NavBar from './components/NavBar/NavBar'
 import PricingOptions from './components/PricingOptions/PricingOptions'
 import ResultChart from './components/ResultChart/ResultChart'
+import axios from 'axios'
+import MarksChart from './components/MarksChart/MarksChart'
 
 
 const pricingPromise = fetch('pricingData.json').then(res => res.json());
+const marksPromise = axios.get('marksData.json')
+
 
 function App() {
   
 
   return (
     <>
-    
+
     <header>
       <NavBar></NavBar>
     {/* <DaisyNav></DaisyNav> */}
@@ -24,9 +28,12 @@ function App() {
         </Suspense>
     </main>
 
-      <ResultChart>
-        
-      </ResultChart>
+      <Suspense fallback={<span className="loading loading-ring loading-lg"></span>}>
+      
+        <MarksChart marksPromise = {marksPromise}></MarksChart>
+      </Suspense>
+
+      <ResultChart></ResultChart>
 
     </>
   )
